3ca2d7a86d25443016af514459265f3e5589b8d9
HarveshvaranS <harveshvaran96@gmail.com>
2018-09-27 17:05:24 +0530

    Angular 6 FTP project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ EmployeeService ]
 })
-export class AppComponent {
-  title = 'lm-app';
+export class AppComponent implements OnInit {
+  constructor(private employeeService: EmployeeService) { }
+
+  title = 'Leave Management Application';
+  employees: Employee[];
+
+  getEmployees(): void {
+      this.employeeService.getEmployees().subscribe(employee=>this.employees=employee);
+     
+  }
+   ngOnInit(): void {
+    this.getEmployees();
+  }
 }
